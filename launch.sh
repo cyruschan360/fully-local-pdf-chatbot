12c8365446ae26b19646ba9b12c8eddcc6e4b87d
Cyrus Chan <cyrus.chan@star.hku.hk>
2024-02-14 23:13:28 +0000

    Fix voy-search version

--- launch.sh
+++ launch.sh
@@ -3,5 +3,9 @@
 #nohup npm start &
 #disown -h
 
-OLLAMA_ORIGINS=https://chatpdf.talic.hku.hk OLLAMA_HOST=gpu1.talic.hku.hk:11435 ollama serve
-OLLAMA_HOST=127.0.0.1:11435 ollama pull mixtral:8x7b-instruct-v0.1-q3_K_M
+#ollama run mixtral:8x7b-instruct-v0.1-q3_K_M
+nohup OLLAMA_ORIGINS=https://chatpdf.talic.hku.hk OLLAMA_HOST=gpu1.talic.hku.hk:11435 ollama serve &
+disown -h
+nohup OLLAMA_HOST=gpu1.talic.hku.hk:11435 ollama pull mixtral:8x7b-instruct-v0.1-q3_K_M &
+disown -h
+
