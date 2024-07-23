36faefc2edc7c11cdf07c35470fbd9645884913b
Cyrus Chan <cyrus.chan@star.hku.hk>
2024-07-23 22:03:47 +0100

    Use Llama 3.1

--- launch.sh
+++ launch.sh
@@ -3,8 +3,8 @@
 #nohup npm start &
 #disown -h
 
-#ollama run mixtral:8x7b-instruct-v0.1-q3_K_M
-#OLLAMA_HOST=gpu1.talic.hku.hk:11435 ollama pull mixtral:8x7b-instruct-v0.1-q3_K_M
+#ollama run llama3.1:8b-instruct-q4_K_M
+#OLLAMA_HOST=gpu1.talic.hku.hk:11435 ollama pull llama3.1:8b-instruct-q4_K_M
 OLLAMA_ORIGINS=https://smartchat.talic.hku.hk OLLAMA_HOST=gpu1.talic.hku.hk:11435 nohup ollama serve &
 disown -h
 
