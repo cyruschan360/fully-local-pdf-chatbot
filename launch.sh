8748abe68f6a5307ae336c53c15fdf8c36afea01
Cyrus Chan <cyrus.chan@star.hku.hk>
2024-07-23 22:25:02 +0100

    Skill ollama startup in launch

--- launch.sh
+++ launch.sh
@@ -5,8 +5,8 @@
 
 #ollama run llama3.1:8b-instruct-q4_K_M
 #OLLAMA_HOST=gpu1.talic.hku.hk:11435 ollama pull llama3.1:8b-instruct-q4_K_M
-OLLAMA_ORIGINS=https://smartchat.talic.hku.hk OLLAMA_HOST=gpu1.talic.hku.hk:11435 nohup ollama serve &
-disown -h
+#OLLAMA_ORIGINS=https://smartchat.talic.hku.hk OLLAMA_HOST=gpu1.talic.hku.hk:11435 nohup ollama serve &
+#disown -h
 
 nohup npm run start &
 disown -h
